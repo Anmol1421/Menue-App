b57c5eac00c1c6d540e66c2584f47740594455b9
Anmol Sharma <97452657+Anmol1421@users.noreply.github.com>
2023-12-01 11:51:18 +0530

    Update Menue.js

--- Menue.js
+++ Menue.js
@@ -1,5 +1,5 @@
 // Import necessary packages
-const express = require('express');
+const express =require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const host = 'localhost'
@@ -12,7 +12,7 @@ const port = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Sample data (initial menu items)
+// Sample data (initial menu items, feedback, private dining, catering, delivery)
 let menuItems = [
     {
         id: 1,
@@ -25,16 +25,14 @@ let menuItems = [
     // Add more menu items as needed
 ];
 
+let feedbacks = []; // Array to store feedback
+let privateDining = {}; // Object to store private dining reservations
+let catering = {}; // Object to store catering service details
+let deliveryOptions = {}; // Object to store delivery options
+
 // Endpoint to get all menu items
 app.get('/menu-items', (req, res) => {
     res.json(menuItems);
-});
-
-// Endpoint to get menu items by category
-app.get('/menu-items/:category', (req, res) => {
-    const category = req.params.category.toLowerCase();
-    const filteredItems = menuItems.filter(item => item.category.toLowerCase() === category);
-    res.json(filteredItems);
 });
 
 // Endpoint to add a new menu item
@@ -44,6 +42,14 @@ app.post('/menu-items', (req, res) => {
     menuItems.push(newItem);
     res.json(newItem);
 });
+
+// Endpoint to get menu items by category
+app.get('/menu-items/:category', (req, res) => {
+    const category = req.params.category.toLowerCase();
+    const filteredItems = menuItems.filter(item => item.category.toLowerCase() === category);
+    res.json(filteredItems);
+});
+
 
 // Endpoint to update a menu item by ID
 app.put('/menu-items/:id', (req, res) => {
@@ -65,6 +71,45 @@ app.delete('/menu-items/:id', (req, res) => {
     const itemId = parseInt(req.params.id);
     menuItems = menuItems.filter(item => item.id !== itemId);
     res.json({ message: 'Menu item deleted successfully' });
+});
+
+// Endpoint to submit feedback
+app.post('/feedback', (req, res) => {
+    const newFeedback = req.body;
+    feedbacks.push(newFeedback);
+    res.json({ message: 'Feedback submitted successfully' });
+});
+
+// Endpoint to make a private dining reservation
+app.post('/private-dining', (req, res) => {
+    const reservationDetails = req.body;
+    // Logic to handle private dining reservations
+    privateDining = reservationDetails;
+    res.json({ message: 'Private dining reservation confirmed' });
+});
+
+// Endpoint to get catering service details
+app.get('/catering', (req, res) => {
+    res.json(catering);
+});
+
+// Endpoint to update catering service details
+app.put('/catering', (req, res) => {
+    const updatedCateringDetails = req.body;
+    catering = updatedCateringDetails;
+    res.json({ message: 'Catering service details updated successfully' });
+});
+
+// Endpoint to get delivery options
+app.get('/delivery', (req, res) => {
+    res.json(deliveryOptions);
+});
+
+// Endpoint to update delivery options
+app.put('/delivery', (req, res) => {
+    const updatedDeliveryOptions = req.body;
+    deliveryOptions = updatedDeliveryOptions;
+    res.json({ message: 'Delivery options updated successfully' });
 });
 
 // Start the server
